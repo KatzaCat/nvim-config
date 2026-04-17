d9e6936abd13266d32e760d6580529a69e5c87ec
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-04-16 23:38:44 -0500

    added a easy way to open a terminal

--- lua/Katze/remap.lua
+++ lua/Katze/remap.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = ";"
 
 vim.keymap.set("n", "<leader>ef", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>lz", vim.cmd.Lazy)
+
+-- Opening a termanal
+vim.keymap.set("n", "<leader>te", ":botright ter<CR>")
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
